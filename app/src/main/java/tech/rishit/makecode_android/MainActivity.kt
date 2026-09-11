053d72dfb19817c0b64f8ef34031eec4506de574
ymoralesborbollon0-oss <ymoralesborbollon0@gmail.com>
2026-09-11 00:05:15 -0600

    Update MainActivity.kt

--- app/src/main/java/tech/rishit/makecode_android/MainActivity.kt
+++ app/src/main/java/tech/rishit/makecode_android/MainActivity.kt
@@ -42,12 +42,12 @@ class MainActivity : AppCompatActivity() {
         myWebView.webViewClient = WebViewClient()
 
         val projectUrl = """
-<div style="position:relative;height:calc(300px + 5em);width:100%;overflow:hidden;">
+<div style="position:relative;height:0;padding-bottom:117.6%;overflow:hidden;">
 <iframe style="position:absolute;top:0;left:0;width:100%;height:100%;"
-src="https://arcade.makecode.com/---codeembed#pub:S78527-42633-44120-54871"
+src="https://arcade.makecode.com/---run?id=S78527-42633-44120-54871"
 allowfullscreen="allowfullscreen"
-frameborder="0"
-sandbox="allow-scripts allow-same-origin"></iframe>
+sandbox="allow-popups allow-forms allow-scripts allow-same-origin"
+frameborder="0"></iframe>
 </div>
 """.trimIndent()
 
